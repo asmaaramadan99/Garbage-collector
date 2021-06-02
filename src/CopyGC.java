3af186a347f861c5f89d01cc3fa68076c233fb51
Mariam Ghazi <mariamghazi9@gmail.com>
2021-06-02 20:57:50 +0200

    Copy

--- src/CopyGC.java
+++ src/CopyGC.java
@@ -1,6 +1,35 @@
+import java.util.ArrayList;
+
 public class CopyGC implements GarbageCollector{
+
+    private ArrayList<INode> roots = new ArrayList<>();
+    private ArrayList<INode> copied = new ArrayList<>();
+
+
+    public ArrayList<INode> getRoots() {
+        return roots;
+    }
+
+    public void setRoots(ArrayList<INode> roots) {
+        this.roots = roots;
+    }
+
     @Override
     public void garbageCollect() {
 
+    }
+    public void copy(){
+        for (int i=0;i<roots.size();i++){
+            copied.add(roots.get(i));
+        }
+        int i=0;
+        while (i < copied.size()){
+            for (int j=0;j< copied.get(i).getChildren().size();j++){
+                INode n=copied.get(i).getChildren().get(j);
+                if (! copied.contains(n))
+                    copied.add(n);
+            }
+            i++;
+        }
     }
 }
